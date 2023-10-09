f0eda6cee7951c14f1860839125534fe4e2f0e5c
kavinprasanth <73733462+kavinsciflash@users.noreply.github.com>
2023-10-09 17:28:59 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 import colors from "colors";
 
 const connectDB = async () => {
+
+  
+// MONGO_URL="mongodb+srv://surbhikumari6061:YC8WrfxL2PnhwjZk@cluster0.fga4ome.mongodb.net/ecommerce"
   try {
     const conn = await mongoose.connect('mongodb+srv://surbhikumari6061:YC8WrfxL2PnhwjZk@cluster0.fga4ome.mongodb.net/ecommerce');
     console.log(`connected to MongoDB Database ${conn.connection.host}`.bgMagenta.white);
